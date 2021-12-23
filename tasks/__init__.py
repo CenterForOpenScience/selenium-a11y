dbbbc5b100d83c91b57f3547563b9bab23b20e30
DougCorell <doug@cos.io>
2021-12-23 13:03:06 -0500

    Add task and yaml file for CAS A11y Tests

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -75,7 +75,7 @@ def test_module(ctx, module=None, params=None):
 
 @task
 def test_ember_accessibility(ctx):
-    """Run the accessibility test for Ember pages on the browser defined by TEST_BUILD.
+    """Run the accessibility tests for Ember pages on the browser defined by TEST_BUILD.
     This task will be invoked after 'ember-osf-web' has been deployed to any testing
     environment.   In order to be included in this test run the test must have the
     pytest marker 'ember_page'.
@@ -87,7 +87,7 @@ def test_ember_accessibility(ctx):
 
 @task
 def test_legacy_accessibility(ctx):
-    """Run the accessibility test for Legacy pages on the browser defined by TEST_BUILD.
+    """Run the accessibility tests for Legacy pages on the browser defined by TEST_BUILD.
     This task will be invoked after 'osf.io' has been deployed to any testing
     environment.   In order to be included in this test run the test must have the
     pytest marker 'legacy_page'.
@@ -105,6 +105,16 @@ def test_preprints_accessibility(ctx):
     """
     file_list = ['tests/test_a11y_preprints.py']
     test_with_retries(ctx, 'Preprints', file_list)
+
+
+@task
+def test_cas_accessibility(ctx):
+    """Run the accessibility test for CAS on the browser defined by TEST_BUILD.
+    This task will be invoked after 'osf-cas' has been deployed to any testing
+    environment.
+    """
+    file_list = ['tests/test_a11y_cas.py']
+    test_with_retries(ctx, 'CAS', file_list)
 
 
 def _get_test_file_list():
